30ae6d3e15f79d45212c3c6d091bf926af533ff8
Arthur Dias <dias.arthurr@gmail.com>
2021-11-19 16:04:09 -0300

    Alterada imagem logo. Finalizado CSS Header

--- components/Header.js
+++ components/Header.js
@@ -7,30 +7,36 @@ import logo from '../public/images/logo.png'
 
 const Header = () => {
   return (
-    <div>
+    <div className={styles.header}>
       <section>
-        <ul>
-          <li>
-            <TiLocationArrowOutline />
-            <p>Avenida Pequeno Príncipe, 0 - Campeche</p>
-            <p>Florianópolis/SC</p>
+        <ul className={styles.header__info}>
+          <li className={styles.item}>
+            <TiLocationArrowOutline className={styles.icon} />
+            <div className={styles.item__info}>
+              <p>Avenida Pequeno Príncipe, 0 - Campeche</p>
+              <p>Florianópolis/SC</p>
+            </div>
           </li>
-          <li>
-            <FiClock />
-            <p>Seg/Sex: 09:00h - 18:00h</p>
-            <p>Sáb/Dom: Plantão</p>
+          <li className={styles.item}>
+            <FiClock className={styles.icon} />
+            <div className={styles.item__info}>
+              <p>Seg/Sex: 09:00h - 18:00h</p>
+              <p>Sáb/Dom: Plantão</p>
+            </div>
           </li>
-          <li>
-            <FiMail />
-            <p>contato@minhaimob.com.br</p>
-            <p>+55 (48) 3322-1234</p>
+          <li className={styles.item}>
+            <FiMail className={styles.icon} />
+            <div className={styles.item__info}>
+              <p>contato@minhaimob.com.br</p>
+              <p>+55 (48) 3322-1234</p>
+            </div>
           </li>
         </ul>
       </section>
-      <section>
+      <section className={styles.header__nav}>
         <Image src={logo} alt='Logo' />
-        <nav>
-          <Link href='/'>Home</Link>
+        <nav className={styles.nav__links}>
+          <Link href='#'>Home</Link>
           <Link href='#'>Destaque</Link>
           <Link href='#'>Alugar</Link>
           <Link href='#'>Comprar</Link>
