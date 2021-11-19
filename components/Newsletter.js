88781054cefb5ca6f020d7764902843c8f5554a3
Arthur Dias <dias.arthurr@gmail.com>
2021-11-19 19:22:10 -0300

    Ajustes cores. Finalizada seção Newsletter

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,7 +1,23 @@
+import styles from '../styles/Newsletter.module.css'
+
 const Newsletter = () => {
   return (
-    <div>
-      <h2>Newsletter</h2>
+    <div className={styles.newsletter}>
+      <div className={styles.newsletter__text}>
+        <p className={styles.text__title}>
+          Quer ficar por dentro das novidades?
+        </p>
+        <p className={styles.text__description}>
+          Deixe o seu nome e seu melhor e-mail nos campos abaixo e nós vamos lhe
+          informar sobre os melhores negócios e todos os lançamentos do sul da
+          ilha
+        </p>
+      </div>
+      <form className={styles.newsletter__form}>
+        <input type='text' placeholder='Digite seu nome' />
+        <input type='email' placeholder='Digite seu melhor e-mail' />
+        <button className={styles.form__button}>Me avise!</button>
+      </form>
     </div>
   )
 }
