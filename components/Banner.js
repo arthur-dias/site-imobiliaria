52ffa847657a84f1b305951cc3094a73cc54497f
Arthur Dias <dias.arthurr@gmail.com>
2021-11-19 16:36:17 -0300

    Adicionada imagem Banner. Finalizado CSS Banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,25 @@
+import Image from 'next/image'
+import banner from '../public/images/banner.jpg'
+import styles from '../styles/Banner.module.css'
+
 const Banner = () => {
   return (
     <div>
-      <h2>Banner</h2>
+      <Image src={banner} alt='banner' />
+      <div className={styles.box}>
+        <p className={styles.box__text}>
+          ENCONTRE O IMÓVEL IDEAL PARA VOCÊ E SUA FAMILIA MORAR NA PRAIA!
+        </p>
+
+        <div className={styles.box__buttons}>
+          <button className={styles.button}>
+            Quero <strong>Comprar</strong>!
+          </button>
+          <button className={styles.button}>
+            Quero <strong>Alugar</strong>!
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
