b1ea0fbf40ca85ed902df5883541e49709e24976
Arthur Dias <dias.arthurr@gmail.com>
2021-11-20 16:52:05 -0300

    Finalizada lógica internas

--- pages/aluguel/[id].js
+++ pages/aluguel/[id].js
@@ -1,7 +1,13 @@
 import { imoveis } from '../../lib/data'
 
 const interna = ({ data }) => {
+  /* imoveis.aluguel.map((imovel) => {
+    const id = imovel.id.toString()
+    console.log(id)
+  }) */
+
   console.log(data)
+
   return (
     <div>
       <p>interna aluguel</p>
@@ -9,25 +15,25 @@ const interna = ({ data }) => {
   )
 }
 
-/* export const getStaticPaths = async () => {
-  const paths = imoveis.aluguel.map((imovel) => ({
-    params: { id: imovel.id.toString() },
-  }))
-
-  return {
-    paths: [],
-    fallback: false,
-  }
-}
-
 export const getStaticProps = async ({ params }) => {
-  const imovel = imovel.filter((p) => p.id.toString() === params.id)
+  const imovel = imoveis.aluguel.filter((p) => p.id.toString() === params.id)
 
   return {
     props: {
       data: imovel[0],
     },
   }
-} */
+}
+
+export const getStaticPaths = async () => {
+  const paths = imoveis.aluguel.map((imovel) => ({
+    params: { id: imovel.id.toString() },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export default interna
--- pages/venda/[id].js
+++ pages/venda/[id].js
@@ -1,11 +1,33 @@
 import { imoveis } from '../../lib/data'
 
-const interna = () => {
+const interna = ({ data }) => {
+  console.log(data)
   return (
     <div>
       <p>interna venda</p>
     </div>
   )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const imovel = imoveis.venda.filter((p) => p.id.toString() === params.id)
+
+  return {
+    props: {
+      data: imovel[0],
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const paths = imoveis.venda.map((imovel) => ({
+    params: { id: imovel.id.toString() },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export default interna
